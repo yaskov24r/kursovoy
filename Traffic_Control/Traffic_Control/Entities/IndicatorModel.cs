8b871d9db47f1ba5e311df5de3ea66f58b7f8409
yaskov24r <funruslanshow@gmail.com>
2024-12-05 22:11:27 +0200

    123
    12

---
+++ Traffic_Control/Traffic_Control/Entities/IndicatorModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Traffic_Control.Entities
+{
+    public class IndicatorModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public string Value { get; set; }
+        public string Unit { get; set; }
+        public List<string> IndicatorValues { get; set; }
+    }
+}
---
+++ Traffic_Control/Traffic_Control/Entities/Sensors/CarSensor.cs
@@ -0,0 +1,23 @@
+﻿using Traffic_Control.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Traffic_Control.Entities.Sensors
+{
+    public class CarSensor : Sensor
+    {
+        public override SensorType Type => SensorType.CarSensor;
+
+        public CarSensor(string name, string description)
+            : base (name, description) { }
+
+        public override void ReadValue()
+        {
+            Value = new Random().Next(20, 60);
+            Console.WriteLine($"{Name} Speed: {Value}");
+        }
+    }
+}
